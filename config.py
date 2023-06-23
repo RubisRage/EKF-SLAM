246a116a749cbccbb4b5d18e92baf61ac1b66b2c
Rubén Santana Lorenzo <rubensanlor@gmail.com>
2023-06-23 17:13:14 +0200

    Refactored corner extraction parameters  into global config file

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import numpy as np
 from math import pi
+from utils import pi_to_pi
 
 log = "medium_nd_5.log"
 dt = 0.1  # seconds
@@ -9,6 +10,26 @@ dt = 0.1  # seconds
 frame_width = 1000
 frame_height = 1000
 meters_to_px_ratio = frame_width / 10
+
+# ===================================
+
+# CORNER EXTRACTION CONFIG ==========
+
+# Line segmentation
+lseg_base_distance = 0.1            # meters
+lseg_distance_scale_factor = 0.05
+lseg_alfa_max = 30*pi / 180         # radians
+lseg_lbd_scale_factor = 0.1
+
+# Line merging
+lmerg_max_distance = 1.5              # meters
+lmerg_max_angle = pi_to_pi(5*pi/180)  # radians
+
+# Corner extraction
+cext_alfa_min = (90-15) * pi / 180
+cext_alfa_max = (90+15) * pi / 180
+cext_dmin = 1
+cext_max_distance_to_corner = 1
 
 # ===================================
 
@@ -39,6 +60,7 @@ R = np.array([
     [0, sigmaB**2]
 ])
 
+# Association gates
 INNER_GATE = 4.0
 OUTER_GATE = 25.0
 
